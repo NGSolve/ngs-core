9b69e5a741c7730cde57002c35edb0da65205bf5
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2015-03-13 10:53:37 +0100

    tm polish

---
+++ src/taskmanager.hpp
@@ -0,0 +1,69 @@
+#ifndef FILE_TASKHANDLER
+#define FILE_TASKHANDLER
+
+/*********************************************************************/
+/* File:   taskhandler.hpp                                           */
+/* Author: M. Hochsterger, J. Schoeberl                              */
+/* Date:   10. Mar. 2015                                             */
+/*********************************************************************/
+
+
+
+
+namespace ngstd
+{
+  
+  class TaskManager
+  {
+
+    class NodeData
+    {
+    public:
+      atomic<int> start_cnt;
+      atomic<int> complete_cnt;
+      atomic<int> participate;
+
+      NodeData() : start_cnt(0), participate(0) { ; }
+    };
+    
+    function<void(int)> func;
+
+    atomic<int> jobnr;
+
+    // atomic<int> complete_cnt;
+    atomic<int> complete[8];   // max nodes
+    atomic<int> done;
+
+    NodeData *nodedata[8];
+
+    int num_nodes;
+    
+  public:
+    
+    TaskManager();
+
+    void CreateTask (function<void(int)> afunc);
+
+    void Done()
+    {
+      done = true;
+    }
+
+    void Loop();
+  };
+
+
+
+
+
+
+
+
+  extern TaskManager * task_manager;
+  
+  void RunWithTaskManager (function<void()> alg);
+}
+
+
+
+#endif
